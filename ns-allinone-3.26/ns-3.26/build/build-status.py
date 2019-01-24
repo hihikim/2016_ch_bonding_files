7b49dd8c53f92cae9c8a735e6a4758ca1688ae92
Seung-seob Lee <shsym1989@gmail.com>
2019-01-25 05:18:18 +0900

    waf configure

--- ns-allinone-3.26/ns-3.26/build/build-status.py
+++ ns-allinone-3.26/ns-3.26/build/build-status.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python
 
 # Programs that are runnable.
-ns3_runnable_programs = ['build/scratch/ns3.26-my_model_test4_2-debug', 'build/scratch/ns3.26-ofdm-vht-validation-debug', 'build/scratch/ns3.26-my_model_test3-debug', 'build/scratch/ns3.26-my_test-debug', 'build/scratch/ns3.26-my_model_test4_3-debug', 'build/scratch/ns3.26-my_model_test3_1-debug', 'build/scratch/ns3.26-my_model_test-debug', 'build/scratch/ns3.26-my_model_test2-debug', 'build/scratch/subdir/ns3.26-subdir-debug', 'build/scratch/ns3.26-scratch-simulator-debug', 'build/scratch/ns3.26-my_model_test4_1-debug', 'build/scratch/ns3.26-my_model_test5-debug', 'build/scratch/ns3.26-vht-wifi-network-debug']
+ns3_runnable_programs = ['build/scratch/ns3.26-my_model_test3-optimized', 'build/scratch/ns3.26-my_model_test-optimized', 'build/scratch/subdir/ns3.26-subdir-optimized', 'build/scratch/ns3.26-my_model_test2-optimized', 'build/scratch/ns3.26-my_model_test4_3-optimized', 'build/scratch/ns3.26-my_model_test5-optimized', 'build/scratch/ns3.26-my_model_test4_2-optimized', 'build/scratch/ns3.26-my_test-optimized', 'build/scratch/ns3.26-my_model_test4_1-optimized', 'build/scratch/ns3.26-vht-wifi-network-optimized', 'build/scratch/ns3.26-scratch-simulator-optimized', 'build/scratch/ns3.26-my_model_test3_1-optimized', 'build/scratch/ns3.26-ofdm-vht-validation-optimized']
 
 # Scripts that are runnable.
 ns3_runnable_scripts = []
