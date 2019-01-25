5bc781a07ba623ecd979c20ad8bee1f6108c5ff2
Seung-seob Lee <shsym1989@gmail.com>
2019-01-25 19:41:02 +0900

    fixed untracked files

--- ns-allinone-3.26/ns-3.26/build/build-status.py
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/env python
-
-# Programs that are runnable.
-ns3_runnable_programs = ['build/scratch/ns3.26-my_model_test3-optimized', 'build/scratch/ns3.26-my_model_test-optimized', 'build/scratch/subdir/ns3.26-subdir-optimized', 'build/scratch/ns3.26-my_model_test2-optimized', 'build/scratch/ns3.26-my_model_test4_3-optimized', 'build/scratch/ns3.26-my_model_test5-optimized', 'build/scratch/ns3.26-my_model_test4_2-optimized', 'build/scratch/ns3.26-my_test-optimized', 'build/scratch/ns3.26-my_model_test4_1-optimized', 'build/scratch/ns3.26-vht-wifi-network-optimized', 'build/scratch/ns3.26-scratch-simulator-optimized', 'build/scratch/ns3.26-my_model_test3_1-optimized', 'build/scratch/ns3.26-ofdm-vht-validation-optimized']
-
-# Scripts that are runnable.
-ns3_runnable_scripts = []
-
